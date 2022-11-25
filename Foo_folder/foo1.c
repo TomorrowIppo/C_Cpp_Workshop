8e020aa72b344fe5a1b8e9525ecb5158efc59fb6
TomorrowIppo <bjk1624@gmail.com>
2022-11-25 23:09:04 +0900

    제발 하자

--- Foo_folder/foo1.c
+++ Foo_folder/foo1.c
@@ -1,33 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
-	char str[50];
-	char *sour, *dest, tmp[50];
-
-	sour = str;
-	dest = tmp;
-
-	//문자열 입력 (공백 포함할 것)
-    printf("문자열을 입력하세요:\n");
-	fgets(str,sizeof(str),stdin);
-    
-	if (str[strlen(str)-1] != 32)
-		str[strlen(str)] = 32;
-					 
+int main() {
+	char str[80];
+	memset(str, 0, sizeof(str));
 	
-    while(*sour)
-	{
-		if(*sour == 32)
-		{
-			*sour++ = '\0';
-		}
-		else
-		{
-		    printf("%s\n", tmp);
-		}
-	}
+	int t;
+	scanf("%d", &t);
+
 	
+
 	return 0;
 }
